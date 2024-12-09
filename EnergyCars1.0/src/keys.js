0d498a3e6897ed051796e5deb360322b5b4c3fd1
Carlos Castillo <carlos.castisho@gmail.com>
2024-12-09 15:22:49 -0300

    cancelar

--- EnergyCars1.0/src/keys.js
+++ EnergyCars1.0/src/keys.js
@@ -2,7 +2,7 @@ module.exports = {
     database: {
         host: 'localhost',
         user: 'root',
-        password: '',
+        password: '94546249_albert',
         database: 'energycars'
     }
 };
